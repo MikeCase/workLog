bb629d8876cd7329026eec481ca24b95aedeb921
MikeCase <michael.e.case@gmail.com>
2021-12-19 17:15:40 -0600

    Vehicle info updates work, properly updating treeview

--- Screens/Content/veh_info_screen.py
+++ Screens/Content/veh_info_screen.py
@@ -5,6 +5,7 @@ import requests
 from db import DB
 
 class VehicleInfoScreen:
+
     def __init__(self, frame) -> None:
         self.db = DB()
         self.padx = 3
@@ -16,33 +17,52 @@ class VehicleInfoScreen:
         self.vehVin = StringVar()
         self.vehMileage = StringVar()
         self.vehPlate = StringVar()
-        self.veh_date_code = StringVar()
+        self.vehDatecode = StringVar()
 
+        self.lblVehYear = ttk.Label(frame, text="Vehicle Year").grid(
+            column=0, row=0, padx=self.padx, pady=self.pady)
+        self.lblVehMake = ttk.Label(frame, text="Vehicle Make").grid(
+            column=0, row=1, padx=self.padx, pady=self.pady)
+        self.lblVehModel = ttk.Label(frame, text="Vehicle Model").grid(
+            column=0, row=2, padx=self.padx, pady=self.pady)
+        self.lblVehEngine = ttk.Label(frame, text="Vehicle Engine").grid(
+            column=0, row=3, padx=self.padx, pady=self.pady)
+        self.lblVehVin = ttk.Label(frame, text="Vehicle Vin").grid(
+            column=3, row=0, padx=self.padx, pady=self.pady)
+        self.lblVehMileage = ttk.Label(frame, text="Vehicle Mileage").grid(
+            column=3, row=1, padx=self.padx, pady=self.pady)
+        self.lblVehPlate = ttk.Label(frame, text="Vehicle Plate").grid(
+            column=3, row=2, padx=self.padx, pady=self.pady)
+        self.lblVehDateCode = ttk.Label(frame, text="Vehicle Date Code").grid(
+            column=6, row=2, padx=self.padx, pady=self.pady)
 
-        self.lblVehYear = ttk.Label(frame, text="Vehicle Year").grid(column=0, row=0, padx=self.padx, pady=self.pady)
-        self.lblVehMake = ttk.Label(frame, text="Vehicle Make").grid(column=0, row=1, padx=self.padx, pady=self.pady)
-        self.lblVehModel = ttk.Label(frame, text="Vehicle Model").grid(column=0, row=2, padx=self.padx, pady=self.pady)
-        self.lblVehEngine = ttk.Label(frame, text="Vehicle Engine").grid(column=0, row=3, padx=self.padx, pady=self.pady)
-        self.lblVehVin = ttk.Label(frame, text="Vehicle Vin").grid(column=3, row=0, padx=self.padx, pady=self.pady)
-        self.lblVehMileage = ttk.Label(frame, text="Vehicle Mileage").grid(column=3, row=1, padx=self.padx, pady=self.pady)
-        self.lblVehPlate = ttk.Label(frame, text="Vehicle Plate").grid(column=3, row=2, padx=self.padx, pady=self.pady)
-        self.lblVehDateCode = ttk.Label(frame, text="Vehicle Date Code").grid(column=6, row=2, padx=self.padx, pady=self.pady)
-
-        self.txtVehYear = ttk.Entry(frame, textvariable=self.vehYear).grid(column=1, row=0, padx=self.padx, pady=self.pady)
-        self.txtVehMake = ttk.Entry(frame, textvariable=self.vehMake).grid(column=1, row=1, padx=self.padx, pady=self.pady)
-        self.txtVehModel = ttk.Entry(frame, textvariable=self.vehModel).grid(column=1, row=2, padx=self.padx, pady=self.pady)
-        self.txtVehEngine = ttk.Entry(frame, textvariable=self.vehEngine).grid(column=1, row=3, padx=self.padx, pady=self.pady)
-        self.txtVehVin = ttk.Entry(frame, textvariable=self.vehVin).grid(column=4, row=0, padx=self.padx, pady=self.pady)
-        self.txtVehMileage = ttk.Entry(frame, textvariable=self.vehMileage).grid(column=4, row=1, padx=self.padx, pady=self.pady)
-        self.txtVehPlate = ttk.Entry(frame, textvariable=self.vehPlate).grid(column=4, row=2, padx=self.padx, pady=self.pady)
-        self.txtVehDateCode = ttk.Entry(frame, textvariable=self.veh_date_code).grid(column=7, row=2, padx=self.padx, pady=self.pady)
-
-        self.btnSaveVehicle = ttk.Button(frame, text='Save', command=lambda: self.saveVehicle()).grid(column=3, row=3, padx=self.padx, pady=self.pady)
-        self.btnClearVehicle = ttk.Button(frame, text='Clear', command=lambda: self.clearVehicle()).grid(column=4, row=3, padx=self.padx, pady=self.pady)
-        self.btnDecodeVin = ttk.Button(frame, text='Decode', command=lambda: self.decodeVin()).grid(column=5, row=3, padx=self.padx, pady=self.pady)
+        self.txtVehYear = ttk.Entry(frame, textvariable=self.vehYear).grid(
+            column=1, row=0, padx=self.padx, pady=self.pady)
+        self.txtVehMake = ttk.Entry(frame, textvariable=self.vehMake).grid(
+            column=1, row=1, padx=self.padx, pady=self.pady)
+        self.txtVehModel = ttk.Entry(frame, textvariable=self.vehModel).grid(
+            column=1, row=2, padx=self.padx, pady=self.pady)
+        self.txtVehEngine = ttk.Entry(frame, textvariable=self.vehEngine).grid(
+            column=1, row=3, padx=self.padx, pady=self.pady)
+        self.txtVehVin = ttk.Entry(frame, textvariable=self.vehVin).grid(
+            column=4, row=0, padx=self.padx, pady=self.pady)
+        self.txtVehMileage = ttk.Entry(frame, textvariable=self.vehMileage).grid(
+            column=4, row=1, padx=self.padx, pady=self.pady)
+        self.txtVehPlate = ttk.Entry(frame, textvariable=self.vehPlate).grid(
+            column=4, row=2, padx=self.padx, pady=self.pady)
+        self.txtVehDateCode = ttk.Entry(frame, textvariable=self.vehDatecode).grid(
+            column=7, row=2, padx=self.padx, pady=self.pady)
+        self.btnSaveVehicle = ttk.Button(frame, text='Save', command=lambda: self.saveVehicle(
+        )).grid(column=3, row=3, padx=self.padx, pady=self.pady)
+        self.btnClearVehicle = ttk.Button(frame, text='Clear', command=lambda: self.clearVehicle(
+        )).grid(column=4, row=3, padx=self.padx, pady=self.pady)
+        self.btnDecodeVin = ttk.Button(frame, text='Decode', command=lambda: self.decodeVin(
+        )).grid(column=5, row=3, padx=self.padx, pady=self.pady)
 
         self.tv = ttk.Treeview(frame)
-        self.tv['columns'] = ('VIN', 'Year', 'Make', 'Model', 'Engine', 'Mileage', 'Plate', 'Datecode')
+
+        self.tv['columns'] = ('VIN', 'Year', 'Make', 'Model',
+                              'Engine', 'Mileage', 'Plate', 'Datecode')
         self.tv.column('#0', width=0, stretch='NO')
         self.tv.column('VIN', anchor='w', width=80)
         self.tv.column('Year', anchor='center', width=10)
@@ -52,7 +72,6 @@ class VehicleInfoScreen:
         self.tv.column('Mileage', anchor='center', width=40)
         self.tv.column('Plate', anchor='center', width=20)
         self.tv.column('Datecode', anchor='w', width=15)
-        
         self.tv.heading('#0', text='', anchor='center')
         self.tv.heading('VIN', text='VIN', anchor='center')
         self.tv.heading('Year', text="Year", anchor='center')
@@ -62,12 +81,25 @@ class VehicleInfoScreen:
         self.tv.heading('Mileage', text="Mileage", anchor='center')
         self.tv.heading('Plate', text="Plate", anchor='center')
         self.tv.heading('Datecode', text="Datecode", anchor='center')
-
-        self.tv.grid(column=0, row=4, columnspan=9, sticky='we')    
-        
+        self.tv.grid(column=0, row=4, columnspan=9, sticky='we')
+        self.tv.bind('<Double-1>', self.onDoubleClick)
         self.listVehicle()
 
+
+# Methods
+    def onDoubleClick(self, event):
+        item = self.tv.selection()[0]
+        self.vehVin.set(value=self.tv.item(item)['values'][0])
+        self.vehYear.set(value=self.tv.item(item)['values'][1])
+        self.vehMake.set(value=self.tv.item(item)['values'][2])
+        self.vehModel.set(value=self.tv.item(item)['values'][3])
+        self.vehEngine.set(value=self.tv.item(item)['values'][4])
+        self.vehMileage.set(value=self.tv.item(item)['values'][5])
+        self.vehPlate.set(value=self.tv.item(item)['values'][6])
+        self.vehDatecode.set(value=self.tv.item(item)['values'][7])
+
     def saveVehicle(self):
+        
         data = {
             'year': str(self.vehYear.get()),
             'make': str(self.vehMake.get()),
@@ -76,11 +108,13 @@ class VehicleInfoScreen:
             'vin': str(self.vehVin.get()),
             'mileage': str(self.vehMileage.get()),
             'plate': str(self.vehPlate.get()),
-            'datecode': str(self.veh_date_code.get()),
+            'datecode': str(self.vehDatecode.get()),
         }
-        print(type(data))
+
+        # print(type(data))
         self.db.addVehicle(data)
-        print('Saving...')
+        # print('Saving...')
+        self._updateVehicleList()
 
     def clearVehicle(self):
         str(self.vehYear.set(''))
@@ -91,20 +125,28 @@ class VehicleInfoScreen:
         str(self.vehMileage.set(''))
         str(self.vehPlate.set(''))
 
+    def _updateVehicleList(self):
+        for i in self.tv.get_children():
+            self.tv.delete(i)
+        vehicle = self.listVehicle()
+        # self.tv.insert('', 'end', values=(vehicle.vin, vehicle.year, vehicle.make,
+        #                vehicle.model, vehicle.engine, vehicle.mileage, vehicle.plate, vehicle.datecode))
+
     def listVehicle(self):
         vehicles = self.db.getVehicle()
         for row in vehicles:
-            self.tv.insert('', 'end', values=(row.vin, row.year, row.make, row.model, row.engine, row.mileage, row.plate, row.datecode))
+            self.tv.insert('', 'end', values=(row.vin, row.year, row.make,
+                           row.model, row.engine, row.mileage, row.plate, row.datecode))
         # self.listVehicle()
 
     def decodeVin(self):
 
         vin = self.vehVin.get()
-        r = requests.get(f'https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvaluesextended/{vin}?format=json')
+        r = requests.get(
+            f'https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvaluesextended/{vin}?format=json')
         result = r.json()['Results'][0]
 
         # print(result['DisplacementL'])
-
         self.vehVin.set(value=result['VIN'])
         self.vehYear.set(value=result['ModelYear'])
         self.vehMake.set(value=result['Make'])
--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 from db_conf import *
+from sqlalchemy import desc
 from datetime import datetime
 from models.vehinfo import VehInfo
 from models.diaginfo import DiagInfo
@@ -17,16 +18,33 @@ class DB:
 
     def addVehicle(self, record):
         # print(record['year'])
-        new_labor = LaborInfo('First job', 'First job', datetime.now())
-        new_record = VehInfo([new_labor], record["vin"], year=record["year"], make=record["make"], model=record["model"],
-                             engine=record["engine"], mileage=record["mileage"], plate=record["plate"], datecode=record["datecode"])
-        # print(new_record.vin)
-        # session.add(new_labor)
-        session.add(new_record)
-        session.commit()
-        print(f'Added {record}')
+        existing_record = session.query(VehInfo).filter_by(vin=record['vin']).first()
+        if existing_record:
+            existing_record.vin = record['vin']
+            existing_record.year = record['year']
+            existing_record.make = record['make']
+            existing_record.model = record['model']
+            existing_record.engine = record['engine']
+            existing_record.mileage = record['mileage']
+            existing_record.plate = record['plate']
+            existing_record.datecode = record['datecode']
+            session.commit()
+            print('Updated Record')
+        else:
+            new_labor = LaborInfo('First job', 'First job', datetime.now())
+            new_record = VehInfo([new_labor], record["vin"], year=record["year"], make=record["make"], model=record["model"],
+                                engine=record["engine"], mileage=record["mileage"], plate=record["plate"], datecode=record["datecode"])
+            # print(new_record.vin)
+            # session.add(new_labor)
+            session.add(new_record)
+            session.commit()
+            print(f'Added {record}')
 
     def getVehicle(self):
-        vehicles = session.query(VehInfo).all()
-        return vehicles
+        vehicle = session.query(VehInfo).all()
+        return vehicle
         
+    def getNewVehicles(self):
+        vehicle = session.query(VehInfo).order_by(desc(VehInfo.id)).first()
+        if vehicle != None:
+            return vehicle
